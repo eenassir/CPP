1e1ca2464146971801abb4f0aa12c228a436eb63
El Mehdi Enassiri <eenassir@e2r8p7.1337.ma>
2025-05-30 09:48:34 +0100

    09

--- cpp_module_09/ex02/main.cpp
+++ cpp_module_09/ex02/main.cpp
@@ -15,6 +15,105 @@
 #include <deque>
 #include <vector>
 
+// template<typename T>
+void g(std::vector<int> &list)
+{
+	// typedef typename T::iterator iter;
+	// iter it = list.begin();
+
+	// for (; it != list.end(); it++)
+	// {
+	// 	std::cout <<*it<<std::endl;
+	// }
+	std::vector< std::vector<int> > l;
+	l.push_back(list);
+	std::cout << *l.begin()->begin();
+
+	// l.push_back(list);
+	// std::cout <<l.begin()->back();
+}
+
+void binary_insertion(std::vector<int> &vect, int nbr)
+{
+	size_t left, right, mid;
+
+	left = 0;
+	right = vect.size();
+
+	while (left < right)
+	{
+		mid = (left + right) / 2;
+		if (vect[mid] == nbr)
+			break ;
+		else if (vect[mid] < nbr)
+			left = mid + 1;
+		else
+			right = mid;
+	}
+	vect.insert(vect.begin() + left, nbr);
+}
+
+void merge(std::vector<int> & vect)
+{
+	if (vect.size() <= 2)
+		return ;
+	int tmp;
+	bool odd = false;
+	if (vect.size() % 2 != 0)
+	{
+		odd = true;
+		tmp = vect.back();
+		vect.pop_back();
+	}
+	std::vector< std::vector<int> > collect;
+	std::vector<int>::iterator it;
+	size_t size = vect.size() / 2, i = 0;
+	while (i < size)
+	{
+		std::vector<int> k;
+		k.push_back(vect.back());
+		vect.pop_back();
+		k.push_back(vect.back());
+		vect.pop_back();
+		collect.push_back(k);
+		i++;
+	}
+	i = 0;
+	for (; i <  collect.size(); i++)
+	{
+		if (collect[i][0] < collect[i][1])
+		{
+			int tmp = collect[i][0];
+			collect[i][0] = collect[i][1];
+			collect[i][1] = tmp;
+		}
+	}
+	std::vector<int> main_chain;
+	std::vector<int> pain_chain;
+	i = 0;
+	std::vector< std::vector<int> >::iterator itt = collect.begin();
+	for (; itt != collect.end(); itt++)
+	{
+		pain_chain.push_back(itt->back());
+		itt->pop_back();
+		main_chain.push_back(itt->back());
+		itt->pop_back();
+	}
+	merge(main_chain);
+	if (main_chain.size() == 2 && (main_chain[0] > main_chain[1]))
+	{
+		int tmp = main_chain[0];
+		main_chain[0] = main_chain[1];
+		main_chain[1] = tmp;
+	}
+	it = pain_chain.begin();
+	for (; it != pain_chain.end(); it++)
+		binary_insertion(main_chain, *it);
+	if (odd == 1)
+		binary_insertion(main_chain, tmp);
+	vect = main_chain;
+}
+
 int main(int ac, char **av)
 {
 	if (ac <= 1)
@@ -61,28 +160,7 @@ int main(int ac, char **av)
 		cont_d.push_back(nbr);
 		cont_v.push_back(nbr);
 	}
-
-	std::deque<int>::iterator it_d;
-	std::vector<int>::iterator it_v;
-
-	it_v = cont_v.begin();
-
-	std::vector< std::vector<int> > contt;
-
-	contt.push_back(cont_v);
-
-	std::vector< std::vector<int> >::iterator itt;
-
-	int tab[7] = {1, 2, 5, 3, 9, 7, 0};
-
-	int i = 0;
-	for (; i < 7 / 2; i++)
-	{
-		std::cout <<tab[i]<<std::endl;
-	}
-	std::cout <<"---------------"<<std::endl;
-	for (;i < 7; i++ )
-	{
-		std::cout <<tab[i]<<std::endl;
-	}
+	merge(cont_v);
+	g(cont_v);
+	// g(cont_d);
 }
